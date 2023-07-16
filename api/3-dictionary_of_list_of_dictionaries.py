14a98dce8c401b1b71d448380aed670bdf7c7993
WENDPANGA1 <w.ima@alustudent.com>
2023-07-16 20:16:36 +0000

    api

--- api/3-dictionary_of_list_of_dictionaries.py
+++ api/3-dictionary_of_list_of_dictionaries.py
@@ -1,42 +1,22 @@
 #!/usr/bin/python3
-"""Module"""
+"""get TODO list"""
 
+import csv
 import json
 import requests
-
-
-def get_employee_task(employee_id):
-    """Doc"""
-    user_url = "https://jsonplaceholder.typicode.com/users/{}" \
-        .format(employee_id)
-
-    user_info = requests.request('GET', user_url).json()
-
-    employee_username = user_info["username"]
-    todos_url = "https://jsonplaceholder.typicode.com/users/{}/todos/" \
-        .format(employee_id)
-    todos_info = requests.request('GET', todos_url).json()
-    return [
-        dict(zip(["task", "completed", "username"],
-                 [task["title"], task["completed"], employee_username]))
-        for task in todos_info]
-
-
-def get_employee_ids():
-    """Doc"""
-    users_url = "https://jsonplaceholder.typicode.com/users/"
-
-    users_info = requests.request('GET', users_url).json()
-    ids = list(map(lambda user: user["id"], users_info))
-    return ids
-
-
-if __name__ == '__main__':
-
-    employee_ids = get_employee_ids()
-
-    with open('todo_all_employees.json', "w") as file:
-        all_users = {}
-        for employee_id in employee_ids:
-            all_users[str(employee_id)] = get_employee_task(employee_id)
-        file.write(json.dumps(all_users)
+import sys
+if __name__ == "__main__":
+    def getTodos(id):
+        link = "https://jsonplaceholder.typicode.com/users/{}/todos".format(id)
+        res = requests.get(link)
+        return json.loads(res.text)
+    link = "https://jsonplaceholder.typicode.com/users/"
+    res = requests.get(link)
+    users = json.loads(res.text)
+    data = {}
+    for i in users:
+        todos = getTodos(i["id"])
+        data[i["id"]] = [{"task": j["title"], "completed": j["completed"],
+                          "username": i["username"]} for j in todos]
+    with open("todo_all_employees.json", 'w', encoding='utf-8') as f:
+        f.write(json.dumps(data))
